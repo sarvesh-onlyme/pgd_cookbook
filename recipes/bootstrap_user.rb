a3b244dba0ee7b4e13a851cbab8cbc74298409d0
sarvesh-onlyme <sarvesh.onlyme@gmail.com>
2014-08-06 22:17:03 +0530

    ignore_failure added

---
+++ recipes/bootstrap_user.rb
@@ -0,0 +1,30 @@
+venv = node['pgd']['virtualenv_path']
+venv_bin = ::File.join(venv, 'bin')
+python = ::File.join(venv_bin, 'python')
+#django_admin = ::File.join(venv_bin, 'django-admin.py')
+manage = ::File.join(node['pgd']['pgd_path'], 'manage.py')
+
+env = {
+  "PGD_CONFIG_DIR" => node['pgd']['virtualenv_path'].to_s ,
+  "DJANGO_SETTINGS_MODULE" => "pgd.settings"
+}
+
+# Use the attributes to bootstrap users if set, otherwise use databag users
+user = node['pgd']['superuser']
+
+username = user['username']
+email = user['email']
+password = user['password']
+
+log "Creating django superuser #{username}"
+
+execute "create_superuser" do
+  command <<-EOS
+  #{python} #{manage} createsuperuser --noinput --username=#{username} --email #{email}
+  #{python} -c \"from django.contrib.auth.models import User;u=User.objects.get(username='#{username}');u.set_password('#{password}');u.save();\"
+  EOS
+  user node['pgd']['user']
+  group node['pgd']['group']
+  environment env
+  ignore_failure true
+end
