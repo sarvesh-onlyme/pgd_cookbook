b7d9b2d5e4bac303dbd7925836257e9d2adbbf2d
ElijahCaine <voigte@osuosl.org>
2015-02-06 16:25:28 -0800

    fixed issue with manage.py and setting up pgd user
    refs #12

--- recipes/bootstrap_user.rb
+++ recipes/bootstrap_user.rb
@@ -6,7 +6,8 @@ manage = ::File.join(node['pgd']['pgd_path'], 'manage.py')
 
 env = {
   "PGD_CONFIG_DIR" => node['pgd']['virtualenv_path'].to_s ,
-  "DJANGO_SETTINGS_MODULE" => "pgd.settings"
+  "DJANGO_SETTINGS_MODULE" => "pgd.settings",
+  "PYTHONPATH" => "/home/vagrant;/home/vagrant/pgd"
 }
 
 # Use the attributes to bootstrap users if set, otherwise use databag users
@@ -23,9 +24,11 @@ execute "create_superuser" do
   command <<-EOS
   #{python} #{manage} createsuperuser --noinput --username=#{username} --email=#{email}
   echo #{python} #{manage} createsuperuser --noinput --username=#{username} --email=#{email}
-  #{python} -c \"from django.contrib.auth.models import User;u=User.objects.get(username='#{username}');u.set_password('#{password}');u.save();\"
+  echo $PYTHONPATH $DJANGO_SETTINGS_MODULE $PGD_CONFIG_DIR
+  echo "from django.contrib.auth.models import User;u=User.objects.get(username='#{username}');u.set_password('#{password}');u.save();" | #{python} #{manage} shell
   EOS
   user node['pgd']['user']
   group node['pgd']['group']
   environment env
+  cwd '/home/vagrant' 
 end
